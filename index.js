f034167d784945bb8fff84bfc883b8f3c2cc9ecd
dalteri <43134233+dalteri@users.noreply.github.com>
2019-08-03 14:38:31 +0300

    Clear text before animate

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 window.onload = function() {
-    fetch("text.txt?mode=nocache").then(data => data.text()).then(data => {
-        animateText(data)
-      });
+  fetch("text.txt?mode=nocache").then(data => data.text()).then(data => {
+    animateText(data)
+  });
 }
 
 function animateText(data) {
-    var ele = document.getElementById("text"),
-        txt = data.split("");
-    var interval = setInterval(function(){
-    if(!txt[0]){
-        return clearInterval(interval);
+  var ele = document.getElementById("text"),
+      txt = data.split("");
+  ele.innerHTML = "";
+  var interval = setInterval(function() {
+    if (!txt[0]) {
+      return clearInterval(interval);
     };
     ele.innerHTML += txt.shift();
-    }, 150);
+  }, 100);
 }
