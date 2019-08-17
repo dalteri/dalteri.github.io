babd377a76451d499f34ac2acbd4296260310a14
dalteri <43134233+dalteri@users.noreply.github.com>
2019-08-17 16:29:49 +0300

    Added ImageLoader class from https://ourcodeworld.com/articles/read/571/how-to-verify-when-multiple-images-have-been-loaded-in-javascript

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var imageCount;
+var imagesLoaded;
+
 window.onload = function() {
   display('menu');
   collapse('loading');
@@ -31,11 +34,17 @@ function startNewGame() {
   document.getElementById('continueGame').onclick = function () {
     closeMenu();
   };
-  Promise.all([fetch('/img/bg1.jpg'), fetch('/img/sprite1.png')]).then(values => {
-    document.getElementById('bg').src = '/img/bg1.jpg';
-    document.getElementById('sprite1').src = '/img/sprite1.png';
+
+  ImageLoader(['img/bg1.jpg','img/sprite1.png'],function () {
+    document.getElementById('bg').src = 'img/bg1.jpg';
+    document.getElementById('sprite1').src = 'img/sprite1.png';
     collapse('loading');
   });
+  // Promise.all([fetch('/img/bg1.jpg'), fetch('/img/sprite1.png')]).then(values => {
+  //   document.getElementById('bg').src = '/img/bg1.jpg';
+  //   document.getElementById('sprite1').src = '/img/sprite1.png';
+  //   collapse('loading');
+  // });
 }
 
 function setNewGameScreen() {
@@ -139,4 +148,54 @@ function closeFullscreen() {
   } else if (document.msExitFullscreen) { /* IE/Edge */
     document.msExitFullscreen();
   }
+}
+
+/**
+ * Loader function that helps to trigger a callback when multiple images has been loaded. Besides
+ * indicates which images were correctly/wrong loaded.
+ *
+ * @param {Array} Images An array of strings with the paths to the images.
+ * @param {Function} Callback Callback function executed when all images has been loaded or not.
+ */
+function ImageLoader(Images, Callback){
+    // Keep the count of the verified images
+    var allLoaded = 0;
+
+    // The object that will be returned in the callback
+    var _log = {
+        success: [],
+        error: []
+    };
+
+    // Executed everytime an img is successfully or wrong loaded
+    var verifier = function(){
+        allLoaded++;
+
+        // triggers the end callback when all images has been tested
+        if(allLoaded == Images.length){
+            Callback.call(undefined, _log);
+        }
+    };
+
+    // Loop through all the images URLs
+    for (var index = 0; index < Images.length; index++) {
+        // Prevent that index has the same value by wrapping it inside an anonymous fn
+        (function(i){
+            // Image path providen in the array e.g image.png
+            var imgSource = Images[i];
+            var img = new Image();
+
+            img.addEventListener("load", function(){
+                _log.success.push(imgSource);
+                verifier();
+            }, false);
+
+            img.addEventListener("error", function(){
+                _log.error.push(imgSource);
+                verifier();
+            }, false);
+
+            img.src = imgSource;
+        })(index);
+    }
 }
